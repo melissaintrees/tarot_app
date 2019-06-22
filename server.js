6e0a79ac411065864544f33bb38b766ad5f808b9
Melissa Computer <melissacomputer@dhcp-10-101-250-203.wireless.northwestern.private>
2019-06-22 13:49:15 -0500

    change config connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 9000;
-var config = require('config');
+var config = require('./config');
 
 app.use(express.static(__dirname + '/public')); // you should change this to be wherever your html files are
 app.use(express.urlencoded({extended: true}));
